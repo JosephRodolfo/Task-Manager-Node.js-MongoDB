7731c9bb0d1235dd8f89d94a5252a534e2efc9a4
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-04-21 17:43:51 -0400

    Add log in post request and tokens to handle log ins

--- src/models/user.js
+++ src/models/user.js
@@ -1,50 +1,103 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
+const mongoose = require("mongoose")
+const validator = require("validator")
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-
-const User = mongoose.model("User", {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    age: {
-      type: Number,
-      default: 0,
-      validate(value) {
-        if (value < 0) {
-          throw new Error("Age must be a positive number");
-        }
-      },
-     
-    },  email: {
-      type: String,
-      required: true,
-      trim: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error("email is invalid");
-        }
-      },
-    },
-      password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        validate(value){
-  
-          if (value.toLowerCase().includes("password")){
-            throw new Error('password must be longer than 6 characters and must not contain "password"')
-          }
-  
-          
-        },
-        trim: true,
-     
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
       }
-  });
+    },
+
+   
+  },  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("email is invalid");
+      }
+    },
+  },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      validate(value){
+
+        if (value.toLowerCase().includes("password")){
+          throw new Error('password must be longer than 6 characters and must not contain "password"')
+        }
+
+        
+      },
+      trim: true,
+   
+    },
+    tokens: [{
+      token: {
+        type: String,
+        required: true
+      }
+    }]
+})
+
+userSchema.methods.generateAuthToken = async function () {
+
+  const user = this
+  const token = jwt.sign({_id: user._id.toString()}, 'thisismynewcourse')
+
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+
+  return token
 
 
+}
 
-  module.exports = User
+userSchema.statics.findByCredentials = async(email, password)=>{
+  const user = await User.findOne({ email });
+
+
+  if (!user) {
+    throw new Error('Unable to log in')
+
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch){
+    throw new Error('Unable to log in')
+  }
+
+  return user
+
+}
+
+//Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+  const user = this
+
+  if (user.isModified('password')) {
+      user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
+
