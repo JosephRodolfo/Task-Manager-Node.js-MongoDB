72fc98cc50f2698045969d4f5137bee84c7f156a
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-04-22 19:01:46 -0400

    Add log out user and log out all user's tokens POST request functionality

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const User = require("../models/user");
+const auth = require('../middleware/auth')
 
 router.get("/test", (req, res) => {
   res.send("From a new file");
@@ -53,13 +54,42 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
+router.post('/users/logout', auth, async (req, res)=>{
+    try {
+      req.user.tokens = req.user.tokens.filter((token) =>{
+        return token.token !== req.token
+      })
+      await req.user.save()
+
+      res.send()
+    } catch (e) {
+      res.status(500).send()
+    }
+
+})
+
+
+router.post('/users/logoutAll', auth, async (req, res)=>{
   try {
-    const users = await User.find({});
-    res.status(201).send(users);
-  } catch (e) {
-    res.status(500).send();
+
+    req.user.tokens = []
+    await req.user.save()
+
+    res.send()
+
+    
+
+  } 
+  
+  
+  catch (e) {
+    res.status(500).send()
   }
+
+})
+
+router.get("/userMe", auth, async (req, res) => {
+  res.send(req.user)
 });
 
 router.patch("/users/:id", async (req, res) => {
